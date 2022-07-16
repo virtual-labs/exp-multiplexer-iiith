651a7f52ce2ed1954408150839b1f4537ceee58e
Shreyash Jain <75749794+shreyash-x@users.noreply.github.com>
2022-07-16 23:54:03 +0530

    Testing (#17)
    * done 2 X 1 multiplexer part
    
    * updated task2 of the multiplexer circuit
    
    * final multiplexer experiment
    
    * Delete experiment directory
    
    * Integrated with demo part. Final.
    
    * Rename simulation.html to demo.html
    
    * Made changes after code review
    
    * Fixed instruction box
    
    * Rewrote animation files
    
    * Fixed typos in posttest and pretest
    
    * Added Fixed procedure, added classes to SVG, handled connection errors.
    
    * Fixed Multiplexer label positioning.
    
    Co-authored-by: mayankbhardwaj719 <83199983+mayankbhardwaj719@users.noreply.github.com>

--- experiment/procedure.md
+++ experiment/procedure.md
@@ -1,6 +1,42 @@
-1. With The help of Theory Page , try to build the Multiplexer Circuit.
-2. First of all build the 2 X 1 Multiplexer.
-3. Then use it to build the 8 X 1 multiplexer.
-4. If You are unable to make it , load or export the circuit directly and use it.
-5. There is an import feature, which lets you construct building blocks. Click on import and then click on circuit board to see the imported block. For more details, refer import section in manual.
+# 2-bit Multiplexer
+
+## Components Required - 
+
+* 2 OR gates
+* 1 AND gate
+* 1 NOT gate
+
+## Circuit Connections - 
+
+* Drag the first OR gate and connect its input points to the input bits A and Select.
+* Drag the NOT gate and connect its input point to the Select input bit.
+* Take another OR gate and connect its input points to the output point of the NOT gate and input bit B.
+* Drag an AND gate and connect its input points to the output points of the two OR gates.
+* Connect output of the AND gate to the output bit in the circuit.
+* After setting the values of A, B, and Select click "Simulate". 
+
+## Observations - 
+
+* When Select is 0 the output is the value of A and when Select is 1 the output is the value of B.
+* If the circuit has been made as described as above, then upon clicking the "Submit" button a "Success" message will be displayed.
+
+# 4-bit Multiplexer
+
+## Components Required - 
+
+* 3 2-bit Multiplexers
+
+## Circuit Connections - 
+
+* Drag the first 2-bit Multiplexer and connect its input points A0 and B0 to the input bits I0 and I1 respectively.
+* Connect its Select input point to the input bit S0.
+* Drag the second 2-bit Multiplexer and connect its input points A0 and B0 to the input bits I2 and I3 respectively.
+* Connect its Select input point to the input bit S0.
+* Drag the third 2-bit Multiplexer and connect its input points A0 and B0 to the output points of first and second Multiplexer respectively.
+* Connect its Select input point to the input bit S1 and its output point to the FinalOutput bit of the circuit.
+
+## Observations - 
+
+* When S1 is 0 the output of the first 2-bit Multiplexer is the output and when S1 is 1 the output of the second 2-bit Multiplexer is the output.
+* If the circuit has been made as described as above, then upon clicking the "Submit" button a "Success" message will be displayed.
 
---
+++ experiment/simulation/js/layout.js
@@ -0,0 +1,166 @@
+import { simulateMux, deleteMux } from "./multiplexer.js"
+import { simulate, deleteElement } from "./gate.js";
+import * as main from "./main.js";
+
+'use strict';
+// Wires
+export const wireColours = [
+  "#ff0000",
+  "#00ff00",
+  "#0000ff",
+  "#bf6be3",
+  "#ff00ff",
+  "#00ffff",
+  "#ff8000",
+  "#00ff80",
+  "#80ff00",
+  "#ff0080",
+  "#8080ff",
+  "#c0c0c0",
+];
+
+
+// Contextmenu
+const menu = document.querySelector(".menu");
+const menuOption = document.querySelector(".menu-option");
+let menuVisible = false;
+
+const toggleMenu = command => {
+  menu.style.display = command === "show" ? "block" : "none";
+  menuVisible = !menuVisible;
+};
+
+export const setPosition = ({ top, left }) => {
+  menu.style.left = `${left}px`;
+  menu.style.top = `${top}px`;
+  toggleMenu("show");
+};
+
+window.addEventListener("click", () => {
+  if (menuVisible) toggleMenu("hide");
+  window.selectedComponent = null;
+  window.componentType = null;
+});
+
+menuOption.addEventListener("click", e => {
+  if (e.target.innerHTML === "Delete") {
+    if (window.componentType === "gate") {
+      deleteElement(window.selectedComponent);
+    }
+    else if (window.componentType === "multiplexer") {
+      deleteMux(window.selectedComponent);
+    }
+  }
+  window.selectedComponent = null;
+  window.componentType = null;
+});
+
+// Tabs
+function changeTabs(e) {
+  const task = e.target.parentNode.id;
+  if (window.currentTab === task) {
+    return;
+  }
+
+  if (window.currentTab != null) {
+    document.getElementById(window.currentTab).classList.remove("is-active");
+  }
+  window.currentTab = task;
+  document.getElementById(task).classList.add("is-active");
+
+  // 2-bit multiplexer
+  if (task === "task1") {
+    main.unbindEvent();
+    main.connectGate();
+    main.refreshWorkingArea();
+    main.initTwoBitMultiplexer();
+    window.simulate = simulate
+  }
+  else if (task === "task2") {
+    main.unbindEvent();
+    main.connectMultiplexer();
+    main.refreshWorkingArea();
+    main.initFourBitMultiplexer();
+    window.simulate = simulateMux;
+  }
+  updateInstructions();
+  updateToolbar();
+  clearObservations();
+  resize();
+
+}
+
+window.changeTabs = changeTabs;
+
+// Instruction box
+const updateInstructions = () => {
+  const task = window.currentTab;
+  const instructionBox = document.getElementById("instruction-title");
+  let title = ""; 
+  if (task === "task1") {
+    title = `Instructions<br>Implement a 2 x 1 Multiplexer using logic gates`;
+  } else if (task === "task2") {
+    title = `Instructions<br>Implement a 4 x 1 Multiplexer using 2 x 1 Multiplexers`;
+  }
+  instructionBox.innerHTML = title;
+}
+
+
+// Toolbar
+function updateToolbar() {
+  let elem = "";
+  if (window.currentTab === "task1") {
+    elem = '<div class="component-button and" onclick="addGate(event)">AND</div><div class="component-button or" onclick="addGate(event)">OR</div><div class="component-button not" onclick="addGate(event)">NOT</div><div class="component-button nand" onclick="addGate(event)">NAND</div><div class="component-button nor" onclick="addGate(event)">NOR</div><div class="component-button xor" onclick="addGate(event)">XOR</div><div class="component-button xnor" onclick="addGate(event)">XNOR</div>'
+  }
+  else if (window.currentTab === "task2") {
+    elem = '<div class="component-button multiplexer" onclick="addMux(event)"></div>'
+  }
+
+  document.getElementById("toolbar").innerHTML = elem;
+}
+
+// Clear observations
+function clearObservations() {
+
+  document.getElementById("table-body").innerHTML = "";
+  let head = ''
+
+  if (window.currentTab === "task1") {
+    head = '<tr><th colspan="3">Inputs</th><th colspan="1">Expected Values</th><th colspan="1">Observed Values</th></tr><tr><th>A</th><th>B</th><th>Select</th><th>Output</th><th>Output</th></tr>'
+  }
+  else if (window.currentTab === "task2") {
+    head = '<tr><th colspan="2">Select</th><th colspan="2">Output</th></tr><tr><th>S0</th><th>S1</th><th>Expected</th><th>Observed</th>'
+  }
+
+  document.getElementById("table-head").innerHTML = head;
+  document.getElementById('result').innerHTML = "";
+
+}
+
+// Making webpage responsive
+
+// Dimensions of working area
+const circuitBoard = document.getElementById("circuit-board");
+// Distance of working area from top
+const circuitBoardTop = circuitBoard.offsetTop;
+// Full height of window
+const windowHeight = window.innerHeight;
+const width = window.innerWidth;
+if (width < 1024) {
+  circuitBoard.style.height = 600 + "px";
+} else {
+  circuitBoard.style.height = windowHeight - circuitBoardTop - 20 + "px";
+}
+
+function resize() {
+  const circuitBoard = document.getElementById("circuit-board");
+  const sidePanels = document.getElementsByClassName("v-datalist-container");
+
+  if (width >= 1024) {
+    for (let i = 0; i < sidePanels.length; i++) {
+      sidePanels[i].style.height = circuitBoard.style.height;
+    }
+  }
+}
+
+resize();
