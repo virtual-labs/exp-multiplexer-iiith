5777f2d099270ddc36719fd27ec9bb4750d13aa6
Ananya Vaibhavi Pabba <anivaibh24@gmail.com>
2023-05-23 18:02:38 +0530

    Solves Issue 20(Sets i/o bit values to default after submitting.

--- experiment/simulation/js/gate.js
+++ experiment/simulation/js/gate.js
@@ -299,6 +299,25 @@ export function submitCircuit() {
       "Output-8"
     );
   }
+
+      // Refresh the input bit values to default 1 and output bit values to default empty black circles after submitting
+      for (let gateId in gates) {
+        const gate = gates[gateId];
+        if (gate.isInput) {
+            gate.setOutput(true);
+            let element = document.getElementById(gate.id);
+            element.className = "high";
+            element.childNodes[0].innerHTML = "1";
+        }
+
+        if(gate.isOutput)
+        {
+            gate.setOutput(null);
+            let element = document.getElementById(gate.id);
+            element.className = "output";
+            element.childNodes[0].innerHTML = "";
+        }
+    }
 }
 window.submitCircuit = submitCircuit;
 
